66b38d074a45474acfeb5d436c595f40ff32aab3
MinMax <MinMax_imum@qq.com>
2026-01-13 03:54:48 +0800

    Initial commit: 添加分辨率展示工具及相关文档

---
+++ debug draw_resolutions.bat
@@ -0,0 +1,15 @@
+
+@ECHO OFF
+TITLE Debug Resolution Display Tool
+ECHO. 
+ECHO Python is running...
+ECHO. 
+ECHO Resolutions Drawer is running...
+ECHO. 
+python ".\draw_resolutions.py"
+ECHO. 
+ECHO The program has stopped running.
+ECHO. 
+ECHO Has reached the end of file.
+ECHO. 
+PAUSE
---
+++ run draw_resolutions.bat
@@ -0,0 +1,12 @@
+
+@ECHO OFF
+TITLE Run Resolution Display Tool
+ECHO. 
+ECHO Python is running...
+ECHO. 
+ECHO Resolutions Drawer is running...
+ECHO. 
+START /B pythonw ".\draw_resolutions.py"
+ECHO. 
+ECHO Has reached the end of file.
+ECHO. 
